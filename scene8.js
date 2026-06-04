d67f8545b5d97113eb04066fcfa4ff16cd1e207e
Deven-cmyk <dagnihot@uwaterloo.ca>
2026-06-04 09:48:26 -0400

    first version

---
+++ scene8.js
@@ -0,0 +1,35 @@
+function drawScene8() {
+  push();
+  background(10, 20, 40);
+  fill(255, 255, 200);
+  ellipse(700, 100, 80, 80);
+  fill(80, 90, 100);
+  triangle(200, 600, 800, 600, 500, 200);
+  pop();
+
+  drawDiogenes(500, 180, "cauldron");
+
+  push();
+  stroke(100);
+  strokeWeight(5);
+  line(500, 160, 550, 120);
+  fill(50);
+  noStroke();
+  rect(540, 110, 30, 20);
+  pop();
+
+  push();
+  textAlign(CENTER);
+  textSize(28);
+  fill(0, 255, 100);
+  text("ENDING: The True Ascent", width / 2, 80);
+  fill(255);
+  textSize(16);
+  text(
+    "Perfect synergy. The hammer hooks ledges, the cauldron deflects rocks.",
+    width / 2,
+    130,
+  );
+  text("Diogenes begins his grueling, legendary ascent.", width / 2, 160);
+  pop();
+}
